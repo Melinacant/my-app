0b33a4afbbd0d50ca0c982075046d068761d6fa8
Melina <83986218+Melinacant@users.noreply.github.com>
2021-09-28 20:31:01 -0300

    Add components

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <nav className="NavBar">
+      <ul>
+        <li>Inicio</li>
+        <li>Productos</li>
+        <li>Información</li>
+        <li>Contacto</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
