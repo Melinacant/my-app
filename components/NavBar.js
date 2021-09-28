9424b8e2e81c73050349ffa4ca385e66a82d4292
Melina <83986218+Melinacant@users.noreply.github.com>
2021-09-27 22:17:50 -0300

    NavBar

---
+++ components/NavBar.js
@@ -0,0 +1,16 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <nav className="NavBar">
+      <ul>
+        <li>Inicio</li>
+        <li>Productos</li>
+        <li>Información</li>
+        <li>Contacto</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
